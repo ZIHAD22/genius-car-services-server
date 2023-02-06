9eec1bdc2cce7ba38d51c1eb97fff3bca03fe731
ZIHAD22 <mdzihad2244@gmail.com>
2023-02-06 14:44:17 +0600

    updating

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 // app
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 // middleware
 app.use(cors())
@@ -103,7 +103,7 @@ const dataBaseConnecting = async () => {
         const userOrder = await curser.toArray()
         res.send(userOrder)
       } else {
-        res.status(403).send({ message: 'Forbidden Access' })
+        res.status(403).send({ message: 'Forbidden Accesse' })
       }
     })
 
